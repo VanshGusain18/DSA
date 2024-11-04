b783c87813b82e201e6e2f7d04e3351337d7cd0e
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-04 11:14:52 +0530

    Missing Number 2

--- Array/Easy/Missing Number.cpp
+++ Array/Easy/Missing Number.cpp
@@ -1,10 +1,27 @@
-int missingNumber(vector<int> &a, int N)
+class Solution
 {
-    // Write your code here.
-    int s = 0;
-    for (int i = 0; i < a.size(); i++)
+public:
+    int missingNumber(vector<int> &nums)
     {
-        s += a[i];
+        int x1 = 0, x2 = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            x1 = x1 ^ (nums[i]);
+            x2 = x2 ^ i;
+        }
+        x2 = x2 ^ n;
+        return x1 ^ x2;
     }
-    return ((N * (N + 1) / 2) - s);
-}
+
+    int missingNumber(vector<int> &a, int N)
+    {
+        // Write your code here.
+        int s = 0;
+        for (int i = 0; i < a.size(); i++)
+        {
+            s += a[i];
+        }
+        return ((N * (N + 1) / 2) - s);
+    }
+};
