b9a76f425cbbf0894ac779f81cb2e8843bbf63b0
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-09 10:35:57 +0530

    Longest Subarray With Sum K

--- Array/Easy/Longest Subarray With Sum K.cpp
+++ Array/Easy/Longest Subarray With Sum K.cpp
@@ -18,3 +18,29 @@ int longestSubarrayWithSumK(vector<int> a, long long k)
     }
     return maxlen;
 }
+
+// Better Approch
+
+#include <map>
+int longestSubarrayWithSumK(vector<int> a, long long k)
+{
+    // Write your code here
+    int len = 0;
+    long long sum = 0;
+    map<long long, int> preSum;
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += a[i];
+        if (sum == k)
+        {
+            len = max(len, i + 1);
+        }
+        long long rem = sum - k;
+        if (preSum.find(rem) != preSum.end())
+        {
+            len = max(len, i - preSum[rem]);
+        }
+        preSum[sum] = i;
+    }
+    return len;
+}
