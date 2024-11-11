3b8b034c94f821b3e03d4dde62783f9581dbb26e
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-11 20:57:57 +0530

    Majority Element

--- Array/Medium/Majority Element.cpp
+++ Array/Medium/Majority Element.cpp
@@ -20,3 +20,39 @@ public:
         return -1;
     }
 };
+
+// Optimal Approch (Moore’s Voting Algorithm)
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        int ele, cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (cnt == 0)
+            {
+                ele = nums[i];
+            }
+            if (nums[i] == ele)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+        cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (ele == nums[i])
+                cnt++;
+            if (cnt > n / 2)
+                return nums[i];
+        }
+        return -1;
+    }
+};
