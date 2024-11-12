38ce1948d63818bcd1c069af9ce1e9318d606060
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-12 19:11:05 +0530

    Maximum Subarray

--- Array/Medium/Maximum Subarray.cpp
+++ Array/Medium/Maximum Subarray.cpp
@@ -25,3 +25,28 @@ public:
         return max;
     }
 };
+
+// Better Approch
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int curr = INT_MIN, max = INT_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            curr = 0;
+            for (int j = i; j < n; j++)
+            {
+                curr += nums[j];
+                if (curr > max)
+                {
+                    max = curr;
+                }
+            }
+        }
+        return max;
+    }
+};
