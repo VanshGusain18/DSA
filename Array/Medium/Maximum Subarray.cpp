3a4dd79b6970879942511dcb39253da66f24a680
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-13 20:14:56 +0530

    Maximum Subarray

--- Array/Medium/Maximum Subarray.cpp
+++ Array/Medium/Maximum Subarray.cpp
@@ -50,3 +50,28 @@ public:
         return max;
     }
 };
+
+// Optimal Approch (Kadane's Algorithm)
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int curr = INT_MIN, max = INT_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (curr < 0)
+            {
+                curr = 0;
+            }
+            curr += nums[i];
+            if (curr > max)
+            {
+                max = curr;
+            }
+        }
+        return max;
+    }
+};
