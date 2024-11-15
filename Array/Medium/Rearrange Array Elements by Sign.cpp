ac42fb65f3b64963cb8fd2ab7a31149bfc055cfc
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-15 20:55:09 +0530

    Rearrange Array Elements by Sign

--- Array/Medium/Rearrange Array Elements by Sign.cpp
+++ Array/Medium/Rearrange Array Elements by Sign.cpp
@@ -35,3 +35,29 @@ public:
         return nums;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    vector<int> rearrangeArray(vector<int> &nums)
+    {
+        vector<int> ans(nums.size());
+        int j = 0, k = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                ans[j] = nums[i];
+                j += 2;
+            }
+            else
+            {
+                ans[k] = nums[i];
+                k += 2;
+            }
+        }
+        return ans;
+    }
+};
