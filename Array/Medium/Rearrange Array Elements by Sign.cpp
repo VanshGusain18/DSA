3c34829f5ea84fe6202e7e0c7cf8734100f79ff5
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-15 21:08:32 +0530

    Rearrange Array Elements by Sign

--- Array/Medium/Rearrange Array Elements by Sign.cpp
+++ Array/Medium/Rearrange Array Elements by Sign.cpp
@@ -61,3 +61,61 @@ public:
         return ans;
     }
 };
+
+// For uneven numbers of neg and positive.
+
+vector<int> RearrangebySign(vector<int> A, int n)
+{
+
+    vector<int> pos;
+    vector<int> neg;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (A[i] > 0)
+            pos.push_back(A[i]);
+        else
+            neg.push_back(A[i]);
+    }
+
+    if (pos.size() < neg.size())
+    {
+
+        for (int i = 0; i < pos.size(); i++)
+        {
+
+            A[2 * i] = pos[i];
+            A[2 * i + 1] = neg[i];
+        }
+
+        int index = pos.size() * 2;
+        for (int i = pos.size(); i < neg.size(); i++)
+        {
+
+            A[index] = neg[i];
+            index++;
+        }
+    }
+
+    else
+    {
+
+        for (int i = 0; i < neg.size(); i++)
+        {
+
+            A[2 * i] = pos[i];
+            A[2 * i + 1] = neg[i];
+        }
+
+        // Fill the remaining positives at the end of the array.
+        int index = neg.size() * 2;
+        for (int i = neg.size(); i < pos.size(); i++)
+        {
+
+            A[index] = pos[i];
+            index++;
+        }
+    }
+    return A;
+};
