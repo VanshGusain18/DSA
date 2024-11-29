a005a46a2f7284a4f52121f3d502915f26d5a918
[vanshgusain] <vanshgusain18@gmail.com>
2024-11-29 22:30:10 +0530

    Subarray Sum Equal K

---
+++ Array/Medium/Subarray Sum Equals K.cpp
@@ -0,0 +1,22 @@
+// Optimal Approch
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        map<long long, int> mpp;
+        long long preSum = 0;
+        int cnt = 0;
+        mpp[0] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            preSum += nums[i];
+            int remove = preSum - k;
+            cnt += mpp[remove];
+            mpp[preSum] += 1;
+        }
+        return cnt;
+    }
+};
