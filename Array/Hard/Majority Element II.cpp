51ef444f15ab7b2c748132d4b774be176c7bd02f
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-07 13:45:09 +0530

    Majority Element II

---
+++ Array/Hard/Majority Element II.cpp
@@ -0,0 +1,34 @@
+// Brute Force Approch
+
+class Solution
+{
+public:
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> list;
+        for (int i = 0; i < n; i++)
+        {
+            if (list.size() == 0 || list[0] != nums[i])
+            {
+                int cnt = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt > n / 3)
+                {
+                    list.push_back(nums[i]);
+                }
+                if (list.size() == 2)
+                {
+                    return list;
+                }
+            }
+        }
+        return list;
+    }
+};
