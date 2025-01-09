d93290e60b1c40b8130314fee21a0280361c248a
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-09 19:03:50 +0530

    3sum

--- Array/Hard/3sum.cpp
+++ Array/Hard/3sum.cpp
@@ -55,3 +55,56 @@ public:
         return result;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        vector<vector<int>> result;
+        while (i < n)
+        {
+            if (i != 0 && nums[i] == nums[i - 1])
+            {
+                i++;
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0)
+                {
+                    j++;
+                }
+                else if (sum > 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    result.push_back(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                    {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1])
+                    {
+                        k--;
+                    }
+                }
+            }
+            i++;
+        }
+        return result;
+    }
+};
