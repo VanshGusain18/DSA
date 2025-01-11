61945ea9ce79c28a175b264c36e926515321113f
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-11 19:15:29 +0530

    Largest subarray with 0 sum

--- Array/Hard/Largest subarray with 0 sum.cpp
+++ Array/Hard/Largest subarray with 0 sum.cpp
@@ -28,3 +28,43 @@ public:
         return count;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    int maxLen(vector<int> &arr)
+    {
+        // code here
+        map<int, int> mp;
+        int count = 0;
+        int sum = 0;
+        int temp = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            if (sum == 0)
+            {
+                temp = i + 1;
+            }
+            else
+            {
+                if (mp.find(sum) != mp.end())
+                {
+                    temp = i - mp[sum];
+                }
+                else
+                {
+                    mp[sum] = i;
+                }
+            }
+            if (count < temp)
+            {
+                count = temp;
+            }
+        }
+        return count;
+    }
+};
