5d8f958c0c217e34177aa3d864a4277e34e1b963
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-13 17:33:17 +0530

    Merge Intervals

--- Array/Hard/Merge Intervals.cpp
+++ Array/Hard/Merge Intervals.cpp
@@ -33,3 +33,34 @@ public:
         return result;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
+        vector<int> temp = {intervals[0][0], intervals[0][1]};
+        for (int i = 1; i < n; i++)
+        {
+            if (temp[1] >= intervals[i][0])
+            {
+                if (temp[1] < intervals[i][1])
+                {
+                    temp[1] = intervals[i][1];
+                }
+            }
+            else
+            {
+                result.push_back(temp);
+                temp = {intervals[i][0], intervals[i][1]};
+            }
+        }
+        result.push_back(temp);
+        return result;
+    }
+};
