7701a5ce195bcf801a7145e4358cc25dc4ddf137
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-15 15:06:15 +0530

    Merge Sorted Array

--- Array/Hard/Merge Sorted Array.cpp
+++ Array/Hard/Merge Sorted Array.cpp
@@ -41,6 +41,39 @@ public:
     }
 };
 
+// Optimal Approch
+
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        int left = m - 1;
+        int right = 0;
+        while (left >= 0 && right < n)
+        {
+            if (nums1[left] > nums2[right])
+            {
+                swap(nums1[left], nums2[right]);
+                left--, right++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            nums1[m + i] = nums2[i];
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
+
 // Optimal Approch (Gap Method)
 
 class Solution
