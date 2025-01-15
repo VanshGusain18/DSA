6c65145482629e620d0c157aa19168f77755ac99
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-15 14:52:29 +0530

    Merge Sorted Array

--- Array/Hard/Merge Sorted Array.cpp
+++ Array/Hard/Merge Sorted Array.cpp
@@ -40,3 +40,62 @@ public:
         }
     }
 };
+
+// Optimal Approch (Gap Method)
+
+class Solution
+{
+public:
+    void swapifgreater(vector<int> &nums1, vector<int> &nums2, int index1, int index2)
+    {
+        if (nums1[index1] > nums2[index2])
+        {
+            swap(nums1[index1], nums2[index2]);
+        }
+    }
+
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+        while (gap > 0)
+        {
+            int left = 0;
+            int right = left + gap;
+            while (right < len)
+            {
+                if (left < m && right >= m)
+                {
+                    if ((right - m) < n)
+                    {
+                        swapifgreater(nums1, nums2, left, right - m);
+                    }
+                }
+                else if (left >= m && right >= m)
+                {
+                    if ((left - m) < n && (right - m) < n)
+                    {
+                        swapifgreater(nums2, nums2, left - m, right - m);
+                    }
+                }
+                else if (left < m && right < m)
+                {
+                    swapifgreater(nums1, nums1, left, right);
+                }
+                right++;
+                left++;
+            }
+            if (gap == 1)
+                break;
+            gap = gap / 2 + gap % 2;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            nums1[m + i] = nums2[i];
+        }
+    }
+};
