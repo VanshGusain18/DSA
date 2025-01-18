df0ba1904709341903ed44f471e632fd570a86ca
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-18 16:10:19 +0530

    Maximum Product Subarray

--- Array/Hard/Maximum Product Subarray.cpp
+++ Array/Hard/Maximum Product Subarray.cpp
@@ -45,3 +45,28 @@ public:
         return product;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int product = INT_MIN;
+        int n = nums.size();
+        int pre = 1;
+        int suff = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (pre == 0)
+                pre = 1;
+            if (suff == 0)
+                suff = 1;
+            pre *= nums[i];
+            suff *= nums[n - i - 1];
+            product = max(product, max(pre, suff));
+        }
+        return product;
+    }
+};
