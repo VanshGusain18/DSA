8fdabb2579374a8940e6a8957ad47ea8853bf4ea
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-19 11:29:41 +0530

    Binary Search

--- Binary Search/BS on 1D Arrays/Binary Search.cpp
+++ Binary Search/BS on 1D Arrays/Binary Search.cpp
@@ -20,3 +20,31 @@ public:
         return -1;
     }
 };
+
+// Recursive code
+
+class Solution
+{
+public:
+    int fn(vector<int> nums, int target, int low, int high)
+    {
+        if (high < low)
+            return -1;
+        else
+        {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                return fn(nums, target, mid + 1, high);
+            return fn(nums, target, low, mid - 1);
+        }
+    }
+
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        return fn(nums, target, low, high);
+    }
+};
