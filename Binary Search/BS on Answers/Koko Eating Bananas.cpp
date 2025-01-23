c4a764279c97732c8b8220f583fcf046a776ee04
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-23 19:20:46 +0530

    Koko Eating Bananas

--- Binary Search/BS on Answers/Koko Eating Bananas.cpp
+++ Binary Search/BS on Answers/Koko Eating Bananas.cpp
@@ -20,3 +20,40 @@ public:
         return speed;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    double fn(vector<int> piles, int m, int n)
+    {
+        double result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            float x = (float)piles[i] / m;
+            result += ceil(x);
+        }
+        return result;
+    }
+
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int n = piles.size();
+        int maxi = -1;
+        for (int i = 0; i < n; i++)
+        {
+            maxi = max(maxi, piles[i]);
+        }
+        int low = 0, high = maxi;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (fn(piles, mid, n) <= h)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
