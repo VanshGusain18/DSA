6c7f8e1057b652b659349cfc1cdecba360401392
[vanshgusain] <vanshgusain18@gmail.com>
2025-01-28 16:40:31 +0530

    Kth Missing Positive Number

--- Binary Search/BS on Answers/Kth Missing Positive Number.cpp
+++ Binary Search/BS on Answers/Kth Missing Positive Number.cpp
@@ -16,3 +16,25 @@ public:
         return k;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int miss = arr[mid] - mid - 1;
+            if (miss < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return k + high + 1;
+    }
+};
