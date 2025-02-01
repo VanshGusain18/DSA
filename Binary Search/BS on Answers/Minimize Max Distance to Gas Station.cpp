e8b56e6d6975930a72e57631e210fc5d3ca24415
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-01 11:08:51 +0530

    Minimize Max Distance to Gas Station

--- Binary Search/BS on Answers/Minimize Max Distance to Gas Station.cpp
+++ Binary Search/BS on Answers/Minimize Max Distance to Gas Station.cpp
@@ -28,3 +28,29 @@ double minimiseMaxDistance(vector<int> &arr, int k)
     }
     return maxans;
 }
+
+// Better Approch
+
+#include <bits/stdc++.h>
+double minimiseMaxDistance(vector<int> &arr, int k)
+{
+    // Write your code here.
+    int n = arr.size();
+    vector<int> howmany(n - 1, 0);
+    priority_queue<pair<double, int>> pq;
+    for (int i = 0; i < n - 1; i++)
+    {
+        double seclen = arr[i + 1] - arr[i];
+        pq.push({seclen, i});
+    }
+    for (int i = 0; i < k; i++)
+    {
+        auto tp = pq.top();
+        pq.pop();
+        int ind = tp.second;
+        howmany[ind]++;
+        double seclen = (arr[ind + 1] - arr[ind]) / (howmany[ind] + 1.0);
+        pq.push({seclen, ind});
+    }
+    return pq.top().first;
+}
