f44af9401b9dc56fded982a0a2ac41955b386cab
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-01 16:41:01 +0530

    Median of Two Sorted Arrays

---
+++ Binary Search/BS on Answers/Median of Two Sorted Arrays.cpp
@@ -0,0 +1,49 @@
+// Brute Aproch
+
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<int> ans;
+        int p1 = 0, p2 = 0;
+        while (p1 < n1 && p2 < n2)
+        {
+            if (nums1[p1] < nums2[p2])
+            {
+                ans.push_back(nums1[p1]);
+                p1++;
+            }
+            else
+            {
+                ans.push_back(nums2[p2]);
+                p2++;
+            }
+        }
+        while (p1 < n1)
+        {
+            ans.push_back(nums1[p1]);
+            p1++;
+        }
+        while (p2 < n2)
+        {
+            ans.push_back(nums2[p2]);
+            p2++;
+        }
+        int n = n1 + n2;
+        if (n % 2 == 0)
+        {
+            int a1 = n / 2;
+            int a2 = (n / 2) - 1;
+            double mid = (ans[a1] + ans[a2]) / 2.0;
+            return mid;
+        }
+        else
+        {
+            int a1 = n / 2;
+            return ans[a1] * 1.0;
+        }
+    }
+};
