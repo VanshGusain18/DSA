033df11ebd10249a520044e93d388d7342d9af18
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-12 19:36:29 +0530

    Valid Anagram

--- Strings/Easy/Valid Anagram.cpp
+++ Strings/Easy/Valid Anagram.cpp
@@ -12,3 +12,30 @@ public:
         return s == t;
     }
 };
+
+// Optimal Aproch
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+            return false;
+        int fq[26] = {0}, n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            fq[s[i] - 'a']++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            fq[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (fq[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
