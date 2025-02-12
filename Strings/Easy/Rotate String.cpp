5b8600c105b288091fd0fce6221c8014b0ee0ff7
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-12 19:34:05 +0530

    Rotate String

---
+++ Strings/Easy/Rotate String.cpp
@@ -0,0 +1,22 @@
+// Brute Aproch
+
+class Solution
+{
+public:
+    bool rotateString(string s, string goal)
+    {
+        if (s.length() != goal.length())
+        {
+            return false;
+        }
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s == goal)
+            {
+                return true;
+            }
+            s = s.substr(1) + s[0];
+        }
+        return false;
+    }
+};
