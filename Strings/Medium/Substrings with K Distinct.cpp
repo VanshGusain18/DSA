2594fd7980f162235798d31772424659f17f3811
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-20 15:06:41 +0530

    Substrings with K Distinct

--- Strings/Medium/Substrings with K Distinct.cpp
+++ Strings/Medium/Substrings with K Distinct.cpp
@@ -23,3 +23,36 @@ public:
         return cnt;
     }
 };
+
+// Optimal Aproch
+
+class Solution
+{
+public:
+    int fn(string &s, int k)
+    {
+        if (k == 0)
+            return 0;
+        int left = 0, cnt = 0;
+        unordered_map<char, int> fq;
+        for (int right = 0; right < s.length(); right++)
+        {
+            fq[s[right]]++;
+            while (fq.size() > k)
+            {
+                fq[s[left]]--;
+                if (fq[s[left]] == 0)
+                    fq.erase(s[left]);
+                left++;
+            }
+            cnt += right - left + 1;
+        }
+        return cnt;
+    }
+
+    int countSubstr(string &s, int k)
+    {
+        // code here.
+        return fn(s, k) - fn(s, k - 1);
+    }
+};
