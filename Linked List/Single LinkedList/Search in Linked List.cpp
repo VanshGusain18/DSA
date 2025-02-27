bc2e0bf99dff044bd1118a0b74344b6f6a008a93
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-27 13:53:24 +0530

    Search in Linked List

---
+++ Linked List/Single LinkedList/Search in Linked List.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, Node *head, int key)
+    {
+        // Code here
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            if (temp->data == key)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
