75afbf202c2a12ff594a4b3f3d1ab8a574323395
[vanshgusain] <vanshgusain18@gmail.com>
2025-02-27 13:50:06 +0530

    Count Linked List Nodes

---
+++ Linked List/Single LinkedList/Count Linked List Nodes.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node *head)
+    {
+        // Code here
+        Node *temp = head;
+        int cnt = 0;
+        while (temp != nullptr)
+        {
+            cnt++;
+            temp = temp->next;
+        }
+        return cnt;
+    }
+};
