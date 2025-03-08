7a4fc73a3571d4059b7d479ccb929fa6bed726b7
[vanshgusain] <vanshgusain18@gmail.com>
2025-03-08 19:48:33 +0530

    Linked List Cycle

---
+++ Linked List/Medium Problems of LL/Linked List Cycle.cpp
@@ -0,0 +1,26 @@
+// Aproch 1
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == NULL)
+        {
+            return false;
+        }
+        map<ListNode *, bool> visited;
+        ListNode *temp = head;
+
+        while (temp != NULL)
+        {
+            if (visited[temp] == true)
+            {
+                return true;
+            }
+            visited[temp] = true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
