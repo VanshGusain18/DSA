47f23a3e5c3dc4340e6ea0b7787cfe4c6c3f22cb
[vanshgusain] <vanshgusain18@gmail.com>
2025-03-11 16:57:09 +0530

    Find length of Loop

---
+++ Linked List/Medium Problems of LL/Find length of Loop.cpp
@@ -0,0 +1,33 @@
+// Brute Approch
+
+class Solution
+{
+public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head)
+    {
+        // Code here
+        map<Node *, bool> mpp;
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            if (mpp[temp] == true)
+            {
+                int cnt = 1;
+                Node *temp2 = temp->next;
+                while (temp2 != temp)
+                {
+                    cnt++;
+                    temp2 = temp2->next;
+                }
+                return cnt;
+            }
+            else
+            {
+                mpp[temp] = true;
+                temp = temp->next;
+            }
+        }
+        return 0;
+    }
+};
