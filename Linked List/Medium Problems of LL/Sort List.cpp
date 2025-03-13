302444fb0ad1c63f4188f6daa704779fb1c43d5f
[vanshgusain] <vanshgusain18@gmail.com>
2025-03-13 09:12:20 +0530

    Sort List

--- Linked List/Medium Problems of LL/Sort List.cpp
+++ Linked List/Medium Problems of LL/Sort List.cpp
@@ -22,3 +22,65 @@ public:
         return head;
     }
 };
+
+// Optimal Code
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast != nullptr && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode *mergeList(ListNode *left, ListNode *right)
+    {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *temp = dummy;
+        while (left != nullptr && right != nullptr)
+        {
+            if (left->val < right->val)
+            {
+                temp->next = left;
+                left = left->next;
+            }
+            else
+            {
+                temp->next = right;
+                right = right->next;
+            }
+            temp = temp->next;
+        }
+        if (left != nullptr)
+        {
+            temp->next = left;
+        }
+        if (right != nullptr)
+        {
+            temp->next = right;
+        }
+        return dummy->next;
+    }
+
+    ListNode *sortList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+        ListNode *mid = middleNode(head);
+        ListNode *left = head;
+        ListNode *right = mid->next;
+        mid->next = nullptr;
+        left = sortList(left);
+        right = sortList(right);
+        return mergeList(left, right);
+    }
+};
