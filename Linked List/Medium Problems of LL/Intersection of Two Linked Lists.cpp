e744440a0f516a964f992d6c74abd55935d6ba2e
[vanshgusain] <vanshgusain18@gmail.com>
2025-03-14 17:12:35 +0530

    Intersection of Two Linked Lists

--- Linked List/Medium Problems of LL/Intersection of Two Linked Lists.cpp
+++ Linked List/Medium Problems of LL/Intersection of Two Linked Lists.cpp
@@ -15,3 +15,26 @@ ListNode *intersectionPresent(ListNode *headA, ListNode *headB)
     }
     return NULL;
 }
+
+// Better Approch
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+    unordered_set<ListNode *> st;
+    ListNode *temp = headA;
+    while (temp != nullptr)
+    {
+        st.insert(temp);
+        temp = temp->next;
+    }
+    temp = headB;
+    while (temp != nullptr)
+    {
+        if (st.find(temp) != st.end())
+        {
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return nullptr;
+}
