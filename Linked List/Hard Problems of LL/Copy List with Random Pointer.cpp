0a842cee97ed678ed3ae9599ec8dba0eb6b3150f
[vanshgusain] <vanshgusain18@gmail.com>
2025-04-10 13:38:27 +0530

    Copy List with Random Pointer

--- Linked List/Hard Problems of LL/Copy List with Random Pointer.cpp
+++ Linked List/Hard Problems of LL/Copy List with Random Pointer.cpp
@@ -23,3 +23,44 @@ public:
         return mpp[head];
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            Node *x = new Node(temp->val);
+            x->next = temp->next;
+            temp->next = x;
+            temp = temp->next->next;
+        }
+        temp = head;
+        while (temp != nullptr)
+        {
+            if (temp->random)
+                temp->next->random = temp->random->next;
+            temp = temp->next->next;
+        }
+        temp = head;
+        Node *dummy = new Node(-1);
+        dummy->next = temp->next;
+        Node *res = dummy;
+        while (temp != nullptr)
+        {
+            res->next = temp->next;
+            temp->next = temp->next->next;
+            res = res->next;
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+};
