01e7a4b9f77c85986d5ecf11437d198afd2f3162
[vanshgusain] <vanshgusain18@gmail.com>
2025-04-11 19:55:38 +0530

    Pow(x, n)

--- Recursion [PatternWise]/Get a Strong Hold/Pow(x, n).cpp
+++ Recursion [PatternWise]/Get a Strong Hold/Pow(x, n).cpp
@@ -20,3 +20,33 @@ public:
         return 1 / res;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        double ans = 1.0;
+        long nn = n;
+        if (nn < 0)
+            nn = nn * -1;
+        while (nn != 0)
+        {
+            if (nn % 2 == 0)
+            {
+                x = x * x;
+                nn = nn / 2;
+            }
+            else
+            {
+                ans = ans * x;
+                nn--;
+            }
+        }
+        if (n < 0)
+            return 1 / ans;
+        return ans;
+    }
+};
