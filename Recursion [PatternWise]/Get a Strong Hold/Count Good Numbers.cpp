0f001f6808dc7b63a4071968a03979726b79c55b
[vanshgusain] <vanshgusain18@gmail.com>
2025-04-12 11:51:05 +0530

    Count Good Numbers

---
+++ Recursion [PatternWise]/Get a Strong Hold/Count Good Numbers.cpp
@@ -0,0 +1,50 @@
+// Brute Approch
+
+class Solution
+{
+public:
+    bool isGood(int num, int length)
+    {
+        string str = to_string(num);
+        while (str.length() < length)
+        {
+            str = "0" + str;
+        }
+        for (int i = 0; i < str.length(); i++)
+        {
+            int dig = str[i] - '0';
+            if (i % 2 == 0)
+            {
+                if (dig % 2 != 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (dig == 0 || dig == 1 || dig == 4 || dig == 6 || dig == 8 || dig == 9)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    int countGoodNumbers(long long n)
+    {
+        long long limit = 1;
+        for (int i = 0; i < n; i++)
+        {
+            limit *= 10;
+        }
+        limit -= 1;
+        int cnt = 0;
+        for (int i = 0; i <= limit; i++)
+        {
+            if (isGood(i, n))
+                cnt++;
+        }
+        return cnt;
+    }
+};
