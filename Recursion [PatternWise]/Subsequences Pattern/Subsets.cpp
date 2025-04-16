0d9249250c6a7196489192df1a0bd7c308eb6466
[vanshgusain] <vanshgusain18@gmail.com>
2025-04-16 13:38:03 +0530

    Subsets

--- Recursion [PatternWise]/Subsequences Pattern/Subsets.cpp
+++ Recursion [PatternWise]/Subsequences Pattern/Subsets.cpp
@@ -22,3 +22,30 @@ public:
         return res;
     }
 };
+
+// Recursive Approch
+
+class Solution
+{
+public:
+    void func(vector<int> &nums, vector<int> &temp, vector<vector<int>> &res, int i)
+    {
+        if (i == nums.size())
+        {
+            res.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        func(nums, temp, res, i + 1);
+        temp.pop_back();
+        func(nums, temp, res, i + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        vector<int> temp;
+        func(nums, temp, res, 0);
+        return res;
+    }
+};
