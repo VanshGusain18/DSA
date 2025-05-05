0890975ada89df8e30084719f7ec6ba99ac1866f
[vanshgusain] <vanshgusain18@gmail.com>
2025-05-05 20:30:45 +0530

    Divide Two Integers

---
+++ Bit Manipulation [Concepts & Problems]/Learn Bit Manipulation/Divide Two Integers.cpp
@@ -0,0 +1,22 @@
+// Brute Approch
+
+class Solution
+{
+public:
+    int divide(int dividend, int divisor)
+    {
+        if (dividend <= INT_MIN && divisor == -1)
+            return INT_MAX;
+        long long ldividend = abs((long long)dividend);
+        long long ldivisor = abs((long long)divisor);
+        long long cnt = 0, temp = ldivisor;
+        while (temp <= ldividend)
+        {
+            temp += ldivisor;
+            cnt++;
+        }
+        if (dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0)
+            return -cnt;
+        return cnt;
+    }
+};
