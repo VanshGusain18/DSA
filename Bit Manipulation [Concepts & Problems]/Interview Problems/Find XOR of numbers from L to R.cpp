5945329aef102dac324be9525b007bc594ec86cf
[vanshgusain] <vanshgusain18@gmail.com>
2025-05-06 15:17:27 +0530

    Find XOR of numbers from L to R

--- Bit Manipulation [Concepts & Problems]/Interview Problems/Find XOR of numbers from L to R.cpp
+++ Bit Manipulation [Concepts & Problems]/Interview Problems/Find XOR of numbers from L to R.cpp
@@ -14,3 +14,29 @@ public:
         return x;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    int xrr(int n)
+    {
+        if (n % 4 == 0)
+            return n;
+        else if (n % 4 == 1)
+            return 1;
+        else if (n % 4 == 2)
+            return n + 1;
+        else
+            return 0;
+    }
+
+    int findXOR(int l, int r)
+    {
+        // complete the function here
+        int xl = xrr(l - 1);
+        int xr = xrr(r);
+        return xl ^ xr;
+    }
+};
