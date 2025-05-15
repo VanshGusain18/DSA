245669442f791f51f6ded787eb849d450ceead7a
[vanshgusain] <vanshgusain18@gmail.com>
2025-05-15 19:46:15 +0530

    Trapping Rain Water

--- Stack and Queues/Monotonic Stack And Queue Problems/Trapping Rain Water.cpp
+++ Stack and Queues/Monotonic Stack And Queue Problems/Trapping Rain Water.cpp
@@ -38,3 +38,36 @@ public:
         return ans;
     }
 };
+
+// Better Approch
+
+class Solution
+{
+public:
+    int trap(vector<int> &height)
+    {
+        int ans = 0, n = height.size();
+        vector<int> pmax(n);
+        vector<int> smax(n);
+        pmax[0] = height[0];
+        smax[n - 1] = height[n - 1];
+        for (int i = 1; i < n; i++)
+        {
+            pmax[i] = max(pmax[i - 1], height[i]);
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            smax[i] = max(smax[i + 1], height[i]);
+        }
+        for (int i = 1; i < n - 1; i++)
+        {
+            int lmax = pmax[i];
+            int rmax = smax[i];
+            if (lmax > height[i] && rmax > height[i])
+            {
+                ans += min(lmax, rmax) - height[i];
+            }
+        }
+        return ans;
+    }
+};
