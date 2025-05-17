6584c516925e66f6ccf4403366195fa69859a0ae
[vanshgusain] <vanshgusain18@gmail.com>
2025-05-17 11:02:22 +0530

    Sum of Subarray Ranges

--- Stack and Queues/Monotonic Stack And Queue Problems/Sum of Subarray Ranges.cpp
+++ Stack and Queues/Monotonic Stack And Queue Problems/Sum of Subarray Ranges.cpp
@@ -19,3 +19,112 @@ public:
         return cnt;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    vector<int> ngef(vector<int> arr, int n)
+    {
+        vector<int> temp(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && arr[st.top()] <= arr[i])
+                st.pop();
+            if (st.empty())
+                temp[i] = n;
+            else
+                temp[i] = st.top();
+            st.push(i);
+        }
+        return temp;
+    }
+
+    vector<int> pgef(vector<int> arr, int n)
+    {
+        vector<int> temp(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && arr[st.top()] < arr[i])
+                st.pop();
+            if (st.empty())
+                temp[i] = -1;
+            else
+                temp[i] = st.top();
+            st.push(i);
+        }
+        return temp;
+    }
+
+    long long sumSubarrayMaxs(vector<int> &arr)
+    {
+        int n = arr.size();
+        long long cnt = 0;
+        vector<int> nge = ngef(arr, n);
+        vector<int> pge = pgef(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            long long left = i - pge[i];
+            long long right = nge[i] - i;
+            cnt += 1LL * left * right * arr[i];
+        }
+        return cnt;
+    }
+
+    vector<int> nsef(vector<int> arr, int n)
+    {
+        vector<int> temp(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            if (st.empty())
+                temp[i] = n;
+            else
+                temp[i] = st.top();
+            st.push(i);
+        }
+        return temp;
+    }
+
+    vector<int> psef(vector<int> arr, int n)
+    {
+        vector<int> temp(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+            if (st.empty())
+                temp[i] = -1;
+            else
+                temp[i] = st.top();
+            st.push(i);
+        }
+        return temp;
+    }
+
+    long long sumSubarrayMins(vector<int> &arr)
+    {
+        int n = arr.size();
+        long long cnt = 0;
+        vector<int> nse = nsef(arr, n);
+        vector<int> pse = psef(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            long long left = i - pse[i];
+            long long right = nse[i] - i;
+            cnt += 1LL * left * right * arr[i];
+        }
+        return cnt;
+    }
+
+    long long subArrayRanges(vector<int> &nums)
+    {
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
+    }
+};
