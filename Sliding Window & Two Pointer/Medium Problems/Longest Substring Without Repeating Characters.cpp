77e0def5fbdd11b705f6d5b82c7ce9e62478ba4b
[vanshgusain] <vanshgusain18@gmail.com>
2025-06-30 13:34:51 +0530

    Longest Substring Without Repeating Characters

--- Sliding Window & Two Pointer/Medium Problems/Longest Substring Without Repeating Characters.cpp
+++ Sliding Window & Two Pointer/Medium Problems/Longest Substring Without Repeating Characters.cpp
@@ -22,3 +22,28 @@ public:
         return res;
     }
 };
+
+// Optimal Solution
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int res = 0, n = s.length();
+        unordered_map<char, int> mp;
+        int r = 0, l = 0;
+        while (r < n)
+        {
+            if (mp.find(s[r]) != mp.end() && mp[s[r]] >= l)
+            {
+                l = mp[s[r]] + 1;
+            }
+            mp[s[r]] = r;
+            int len = r - l + 1;
+            res = max(len, res);
+            r++;
+        }
+        return res;
+    }
+};
