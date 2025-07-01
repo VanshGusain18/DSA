6e1f4a022f6b620e9d2dcb7608f99c5dc507c720
[vanshgusain] <vanshgusain18@gmail.com>
2025-07-01 15:40:52 +0530

    Longest Repeating Character Replacement

--- Sliding Window & Two Pointer/Medium Problems/Longest Repeating Character Replacement.cpp
+++ Sliding Window & Two Pointer/Medium Problems/Longest Repeating Character Replacement.cpp
@@ -62,3 +62,41 @@ public:
         return res;
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    int characterReplacement(string s, int k)
+    {
+        int n = s.length(), res = 0;
+        int l = 0, r = 0, mfq = 0;
+        unordered_map<char, int> mp;
+        while (r < n)
+        {
+            mp[s[r]]++;
+            mfq = max(mfq, mp[s[r]]);
+            int len = r - l + 1;
+            int cnv = len - mfq;
+            if (cnv > k)
+            {
+                mp[s[l]]--;
+                mfq = 0;
+                for (auto ele : mp)
+                {
+                    mfq = max(mfq, ele.second);
+                }
+                l++;
+                len = r - l + 1;
+                cnv = len - mfq;
+            }
+            else
+            {
+                res = max(res, len);
+            }
+            r++;
+        }
+        return res;
+    }
+};
