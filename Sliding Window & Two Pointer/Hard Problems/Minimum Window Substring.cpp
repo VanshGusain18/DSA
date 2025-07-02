a07d66c1e278bd50733723250f51a7f6fec42652
[vanshgusain] <vanshgusain18@gmail.com>
2025-07-02 16:33:14 +0530

    Minimum Window Substring

--- Sliding Window & Two Pointer/Hard Problems/Minimum Window Substring.cpp
+++ Sliding Window & Two Pointer/Hard Problems/Minimum Window Substring.cpp
@@ -5,7 +5,7 @@ class Solution
 public:
     string minWindow(string s, string t)
     {
-        int cnt = 0, n = s.length();
+        int n = s.length();
         unordered_map<char, int> mp;
         for (int i = 0; i < t.length(); i++)
         {
@@ -38,6 +38,51 @@ public:
                     break;
                 }
             }
+        }
+        if (idx == -1)
+            return "";
+        string res;
+        for (int i = idx; i < idx + mini; i++)
+        {
+            res += s[i];
+        }
+        return res;
+    }
+};
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    string minWindow(string s, string t)
+    {
+        int n = s.length();
+        unordered_map<char, int> mp;
+        for (int i = 0; i < t.length(); i++)
+        {
+            mp[t[i]]++;
+        }
+        int mini = INT_MAX, idx = -1;
+        int r = 0, l = 0, cnt = 0;
+        while (r < n)
+        {
+            if (mp[s[r]] > 0)
+                cnt++;
+            mp[s[r]]--;
+            while (cnt == t.length())
+            {
+                if (r - l + 1 < mini)
+                {
+                    mini = r - l + 1;
+                    idx = l;
+                }
+                mp[s[l]]++;
+                if (mp[s[l]] > 0)
+                    cnt--;
+                l++;
+            }
+            r++;
         }
         if (idx == -1)
             return "";
