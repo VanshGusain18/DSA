b725727c53dc8e9c2c67f37c4addaed6fb493482
[vanshgusain] <vanshgusain18@gmail.com>
2025-07-02 13:17:31 +0530

    Number of Substrings Containing All Three Characters

---
+++ Sliding Window & Two Pointer/Medium Problems/Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,21 @@
+// Brute Approch
+
+class Solution
+{
+public:
+    int numberOfSubstrings(string s)
+    {
+        int cnt = 0, n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            vector<int> arr(3, 0);
+            for (int j = i; j < n; j++)
+            {
+                arr[s[j] - 'a'] = 1;
+                if (arr[0] + arr[1] + arr[2] == 3)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
