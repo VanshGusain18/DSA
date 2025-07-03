db95082dd12d65ca57b92e8e5e517b488c55fd2b
[vanshgusain] <vanshgusain18@gmail.com>
2025-07-03 13:42:33 +0530

    Jump Game

---
+++ Greedy Algorithms/Medium and Hard Problems/Jump Game.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int mx = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (mx < i)
+                return false;
+            mx = max(mx, nums[i] + i);
+            if (mx >= nums.size() - 1)
+                return true;
+        }
+        return true;
+    }
+};
