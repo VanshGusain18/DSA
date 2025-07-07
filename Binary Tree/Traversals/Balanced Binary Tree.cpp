5358f0d38975253ceed4cc34d5ea305538ff5a5f
[vanshgusain] <vanshgusain18@gmail.com>
2025-07-07 16:19:07 +0530

    Balanced Binary Tree

--- Binary Tree/Traversals/Balanced Binary Tree.cpp
+++ Binary Tree/Traversals/Balanced Binary Tree.cpp
@@ -25,15 +25,40 @@ public:
 
     bool isBalanced(TreeNode *root)
     {
+        if (maxDepth)
+    }
+};
+
+// Optimal Approch
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
         if (root == nullptr)
-            return true;
-        int lefth = maxDepth(root->left);
-        int righth = maxDepth(root->right);
-        if (abs(lefth - righth) > 1)
-            return false;
-        if (!isBalanced(root->left))
-            return false;
-        if (!isBalanced(root->right))
+            return 0;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        if (lh == -1 || rh == -1 || abs(lh - rh) > 1)
+            return -1;
+        return 1 + max(lh, rh);
+    }
+
+    bool isBalanced(TreeNode *root)
+    {
+        if (maxDepth(root) == -1)
             return false;
         return true;
     }
