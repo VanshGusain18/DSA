b22522f000613ee810e452417266cf75c1370c94
[vanshgusain] <vanshgusain18@gmail.com>
2025-07-07 16:31:36 +0530

    Diameter of Binary Tree

--- Binary Tree/Traversals/Diameter of Binary Tree.cpp
+++ Binary Tree/Traversals/Diameter of Binary Tree.cpp
@@ -41,3 +41,37 @@ public:
         return maxi;
     }
 };
+
+// Optimal Approch
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root, int &maxi)
+    {
+        if (root == nullptr)
+            return 0;
+        int lh = maxDepth(root->left, maxi);
+        int rh = maxDepth(root->right, maxi);
+        maxi = max(lh + rh, maxi);
+        return 1 + max(lh, rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        int maxi = 0;
+        maxDepth(root, maxi);
+        return maxi;
+    }
+};
