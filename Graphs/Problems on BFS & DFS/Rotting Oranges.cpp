87b4ff8789b6c57225122497d44882fe66c7ab4c
[vanshgusain] <vanshgusain18@gmail.com>
2025-08-11 10:37:15 +0530

    Rotting Oranges

--- Graphs/Problems on BFS & DFS/Rotting Oranges.cpp
+++ Graphs/Problems on BFS & DFS/Rotting Oranges.cpp
@@ -69,3 +69,57 @@ public:
         return cnt;
     }
 };
+
+// Cleaner Approch
+
+class Solution
+{
+public:
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int r = grid.size(), c = grid[0].size();
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(r, vector<int>(c, 0));
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (grid[i][j] == 2)
+                {
+                    vis[i][j] = 2;
+                    q.push({{i, j}, 0});
+                }
+            }
+        }
+        int drow[] = {1, 0, -1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        int tm = 0;
+        while (!q.empty())
+        {
+            int row = q.front().first.first, col = q.front().first.second;
+            int t = q.front().second;
+            tm = max(tm, t);
+            q.pop();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if (nrow >= 0 && nrow < r && ncol >= 0 && ncol < c && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2)
+                {
+                    q.push({{nrow, ncol}, t + 1});
+                    vis[nrow][ncol] = 2;
+                }
+            }
+        }
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (grid[i][j] == 1 && vis[i][j] != 2)
+                    return -1;
+            }
+        }
+        return tm;
+    }
+};
