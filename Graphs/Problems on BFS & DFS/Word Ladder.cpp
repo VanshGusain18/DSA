dd9473e54b7a68ee3a2d46536cc6a252c221a761
[vanshgusain] <vanshgusain18@gmail.com>
2025-08-13 18:49:02 +0530

    Word Ladder

---
+++ Graphs/Problems on BFS & DFS/Word Ladder.cpp
@@ -0,0 +1,38 @@
+// Brute Approch
+
+class Solution
+{
+public:
+    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    {
+        queue<pair<string, int>> q;
+        set<string> st(wordList.begin(), wordList.end());
+        int n = beginWord.length();
+        q.push({beginWord, 1});
+        while (!q.empty())
+        {
+            string wrd = q.front().first;
+            int len = q.front().second;
+            q.pop();
+            for (int i = 0; i < n; i++)
+            {
+                char original = wrd[i];
+                for (char j = 'a'; j <= 'z'; j++)
+                {
+                    wrd[i] = j;
+                    if (st.find(wrd) != st.end())
+                    {
+                        if (wrd == endWord)
+                        {
+                            return len + 1;
+                        }
+                        q.push({wrd, len + 1});
+                        st.erase(wrd);
+                    }
+                }
+                wrd[i] = original;
+            }
+        }
+        return 0;
+    }
+};
