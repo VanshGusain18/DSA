39ce7023a8cf21a7e9ef3a19262d0de9d7f24168
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-08 19:15:24 +0530

    Fibonacci Number

--- Dynamic Programming/Introduction to DP/Fibonacci Number.cpp
+++ Dynamic Programming/Introduction to DP/Fibonacci Number.cpp
@@ -51,3 +51,23 @@ public:
         return dp[n];
     }
 };
+
+// Optimal Approch
+
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        int prev = 1, prev2 = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
