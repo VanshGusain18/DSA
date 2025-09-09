4936ebc95393b404080180356adfab0ab28114fd
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-09 18:52:59 +0530

    Frog Jump

--- Dynamic Programming/Introduction to DP/Frog Jump.cpp
+++ Dynamic Programming/Introduction to DP/Frog Jump.cpp
@@ -22,3 +22,32 @@ public:
         return fn(height.size() - 1, height);
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(int n, vector<int> &height, vector<int> &dp)
+    {
+        if (n == 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
+        int left = fn(n - 1, height, dp) + abs(height[n] - height[n - 1]);
+        int right = INT_MAX;
+        if (n > 1)
+        {
+            right = fn(n - 2, height, dp) + abs(height[n] - height[n - 2]);
+        }
+        return dp[n] = min(left, right);
+    }
+
+    int minCost(vector<int> &height)
+    {
+        // Code here
+        int n = height.size();
+        vector<int> dp(n, -1);
+        return fn(n - 1, height, dp);
+    }
+};
