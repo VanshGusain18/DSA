28d6d8a6228c105d2ed46e86dfd434f469ca7109
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-09 18:27:15 +0530

    Climbing Stairs

--- Dynamic Programming/Introduction to DP/Climbing Stairs.cpp
+++ Dynamic Programming/Introduction to DP/Climbing Stairs.cpp
@@ -12,3 +12,26 @@ public:
         return left + right;
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(int n, vector<int> &dp)
+    {
+        if (n <= 1)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
+        int left = fn(n - 1, dp);
+        int right = fn(n - 2, dp);
+        return dp[n] = left + right;
+    }
+
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, -1);
+        return fn(n, dp);
+    }
+};
