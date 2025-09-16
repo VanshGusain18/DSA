8458465b63ee03fc3bf9ae258c08e96427736aab
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-16 18:34:39 +0530

    House Robber

--- Dynamic Programming/1D DP/House Robber.cpp
+++ Dynamic Programming/1D DP/House Robber.cpp
@@ -45,3 +45,27 @@ public:
         return fn(nums, dp, n - 1);
     }
 };
+
+// Tabulation Approch
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            int pick = nums[i];
+            if (i > 1)
+            {
+                pick += dp[i - 2];
+            }
+            int nPick = dp[i - 1];
+            dp[i] = max(pick, nPick);
+        }
+        return dp[n - 1];
+    }
+};
