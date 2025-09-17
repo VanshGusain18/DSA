25c47c674b6d0e4d7cb732e37bacf6174ac57d36
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-17 19:05:18 +0530

    Geek's Training

--- Dynamic Programming/2D & 3D DP and DP on Grids/Geek's Training.cpp
+++ Dynamic Programming/2D & 3D DP and DP on Grids/Geek's Training.cpp
@@ -28,3 +28,34 @@ public:
         return fn(arr, n - 1, -1);
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(vector<vector<int>> &arr, vector<vector<int>> &dp, int idx, int last)
+    {
+        if (idx == -1)
+            return 0;
+        if (dp[idx][last + 1] != -1)
+            return dp[idx][last + 1];
+        int maxi = INT_MIN;
+        for (int i = 0; i < 3; i++)
+        {
+            if (i != last)
+            {
+                int curr = arr[idx][i] + fn(arr, dp, idx - 1, i);
+                maxi = max(maxi, curr);
+            }
+        }
+        return dp[idx][last + 1] = maxi;
+    }
+
+    int maximumPoints(vector<vector<int>> &arr)
+    {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+        return fn(arr, dp, n - 1, -1);
+    }
+};
