c78bdd4490329ce49678f6c91909c4d181c09eae
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-18 11:18:57 +0530

    Unique Paths

--- Dynamic Programming/2D & 3D DP and DP on Grids/Unique Paths.cpp
+++ Dynamic Programming/2D & 3D DP and DP on Grids/Unique Paths.cpp
@@ -19,3 +19,28 @@ public:
         return fn(m - 1, n - 1);
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(int r, int c, vector<vector<int>> &dp)
+    {
+        if (r == 0 && c == 0)
+            return 1;
+        if (r < 0 || c < 0)
+            return 0;
+        if (dp[r][c] != -1)
+            return dp[r][c];
+        int up = fn(r - 1, c, dp);
+        int right = fn(r, c - 1, dp);
+        return dp[r][c] = up + right;
+    }
+
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return fn(m - 1, n - 1, dp);
+    }
+};
