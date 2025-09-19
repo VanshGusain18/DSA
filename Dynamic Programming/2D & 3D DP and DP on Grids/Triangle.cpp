bb6d3690b9c9dd231113d6a11524260bdbef3a92
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-19 15:24:02 +0530

    Triangle

--- Dynamic Programming/2D & 3D DP and DP on Grids/Triangle.cpp
+++ Dynamic Programming/2D & 3D DP and DP on Grids/Triangle.cpp
@@ -29,3 +29,38 @@ public:
         return mini;
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(vector<vector<int>> &grid, vector<vector<int>> &dp, int i, int j)
+    {
+        if (i == 0 && j == 0)
+        {
+            return grid[0][0];
+        }
+        if (i < 0 || j < 0 || i < j)
+        {
+            return INT_MAX;
+        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int up = fn(grid, dp, i - 1, j);
+        int left = fn(grid, dp, i - 1, j - 1);
+        return dp[i][j] = grid[i][j] + min(up, left);
+    }
+
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        int n = triangle.size();
+        int mini = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++)
+        {
+            mini = min(mini, fn(triangle, dp, n - 1, i));
+        }
+        return mini;
+    }
+};
