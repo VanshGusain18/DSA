a7a84ab4b464f06bc504b64e39d8463811dbe1ae
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-23 19:20:05 +0530

    Cherry Pickup II

---
+++ Dynamic Programming/2D & 3D DP and DP on Grids/Cherry Pickup II.cpp
@@ -0,0 +1,46 @@
+// Recursive Approch
+
+class Solution
+{
+public:
+    int fn(int i, int j1, int j2, int r, int c, vector<vector<int>> &grid)
+    {
+        if (j1 < 0 || j1 >= c || j2 < 0 || j2 >= c)
+        {
+            return -1e8;
+        }
+        if (i == r - 1)
+        {
+            if (j1 == j2)
+            {
+                return grid[i][j1];
+            }
+            return grid[i][j1] + grid[i][j2];
+        }
+        int dcol[3] = {-1, 0, 1};
+        int maxi = INT_MIN;
+        for (int k = 0; k < 3; k++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                int value = 0;
+                if (j1 == j2)
+                {
+                    value = grid[i][j1] + fn(i + 1, j1 + dcol[k], j2 + dcol[j], r, c, grid);
+                }
+                else
+                {
+                    value = grid[i][j1] + grid[i][j2] + fn(i + 1, j1 + dcol[k], j2 + dcol[j], r, c, grid);
+                }
+                maxi = max(maxi, value);
+            }
+        }
+        return maxi;
+    }
+
+    int cherryPickup(vector<vector<int>> &grid)
+    {
+        int r = grid.size(), c = grid[0].size();
+        return fn(0, 0, c - 1, r, c, grid);
+    }
+};
