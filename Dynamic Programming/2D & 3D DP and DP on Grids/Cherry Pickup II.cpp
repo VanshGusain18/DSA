58ab362470b0ecbece87bd7dbc8929ade55cd937
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-24 18:44:57 +0530

    Cherry Pickup II

--- Dynamic Programming/2D & 3D DP and DP on Grids/Cherry Pickup II.cpp
+++ Dynamic Programming/2D & 3D DP and DP on Grids/Cherry Pickup II.cpp
@@ -94,3 +94,56 @@ public:
         return fn(0, 0, c - 1, r, c, grid, dp);
     }
 };
+
+// Tabulation Method
+
+class Solution
+{
+public:
+    int cherryPickup(vector<vector<int>> &grid)
+    {
+        int r = grid.size(), c = grid[0].size();
+        vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, 0)));
+        for (int j1 = 0; j1 < c; j1++)
+        {
+            for (int j2 = 0; j2 < c; j2++)
+            {
+                if (j1 == j2)
+                    dp[r - 1][j1][j2] = grid[r - 1][j1];
+                else
+                    dp[r - 1][j1][j2] = grid[r - 1][j1] + grid[r - 1][j2];
+            }
+        }
+        int dcol[3] = {-1, 0, 1};
+        for (int i = r - 2; i >= 0; i--)
+        {
+            for (int j1 = 0; j1 < c; j1++)
+            {
+                for (int j2 = 0; j2 < c; j2++)
+                {
+                    int maxi = INT_MIN;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            int nj1 = j1 + dcol[k];
+                            int nj2 = j2 + dcol[j];
+                            if (nj1 >= 0 && nj1 < c && nj2 >= 0 && nj2 < c)
+                            {
+                                int value = 0;
+                                if (j1 == j2)
+                                    value = grid[i][j1] + dp[i + 1][nj1][nj2];
+                                else
+                                    value = grid[i][j1] + grid[i][j2] + dp[i + 1][nj1][nj2];
+                                maxi = max(maxi, value);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][c - 1];
+    }
+};
