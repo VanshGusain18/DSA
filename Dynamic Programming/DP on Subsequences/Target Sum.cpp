3c9851ae2bf0f10d44b04213d728a4fe36034b4e
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-30 21:54:13 +0530

    Target Sum

--- Dynamic Programming/DP on Subsequences/Target Sum.cpp
+++ Dynamic Programming/DP on Subsequences/Target Sum.cpp
@@ -3,24 +3,77 @@
 class Solution
 {
 public:
-    int fn(vector<int> &nums, int target, int idx)
+    int fn(vector<int> &nums, int idx, int target)
     {
         if (idx == 0)
         {
             if (target == 0 && nums[0] == 0)
                 return 2;
-            if (target - nums[0] == 0 || target + nums[0] == 0)
+            if (target == 0 || target == nums[0])
                 return 1;
             return 0;
         }
-        int pos = fn(nums, target + nums[idx], idx - 1);
-        int neg = fn(nums, target - nums[idx], idx - 1);
-        return pos + neg;
+
+        int notTake = fn(nums, idx - 1, target);
+        int take = 0;
+        if (nums[idx] <= target)
+        {
+            take = fn(nums, idx - 1, target - nums[idx]);
+        }
+        return take + notTake;
     }
 
     int findTargetSumWays(vector<int> &nums, int target)
     {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if ((totalSum + target) % 2 != 0 || abs(target) > totalSum)
+            return 0;
+
+        int subsetSum = (totalSum + target) / 2;
         int n = nums.size();
-        return fn(nums, target, n - 1);
+        return fn(nums, n - 1, subsetSum);
+    }
+};
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int countSubsets(vector<int> &nums, int idx, int target, vector<vector<int>> &dp)
+    {
+        if (idx == 0)
+        {
+            if (target == 0 && nums[0] == 0)
+                return 2;
+            if (target == 0 || target == nums[0])
+                return 1;
+            return 0;
+        }
+
+        if (dp[idx][target] != -1)
+            return dp[idx][target];
+
+        int notTake = countSubsets(nums, idx - 1, target, dp);
+        int take = 0;
+        if (nums[idx] <= target)
+        {
+            take = countSubsets(nums, idx - 1, target - nums[idx], dp);
+        }
+
+        return dp[idx][target] = take + notTake;
+    }
+
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if ((totalSum + target) % 2 != 0 || abs(target) > totalSum)
+            return 0;
+
+        int subsetSum = (totalSum + target) / 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(subsetSum + 1, -1));
+
+        return countSubsets(nums, n - 1, subsetSum, dp);
     }
 };
