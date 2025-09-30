2c275d3a76fef718aeeb768e6209720a60ff7f4e
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-30 19:35:22 +0530

    Coin Change

--- Dynamic Programming/DP on Subsequences/Coin Change.cpp
+++ Dynamic Programming/DP on Subsequences/Coin Change.cpp
@@ -29,3 +29,35 @@ public:
         return mini;
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(vector<int> &coins, int amount, vector<vector<int>> &dp, int idx)
+    {
+        if (idx == 0)
+        {
+            if (amount % coins[0] == 0)
+                return amount / coins[0];
+            return 1e9;
+        }
+        if (dp[idx][amount] != -1)
+            return dp[idx][amount];
+
+        int notTaken = fn(coins, amount, dp, idx - 1);
+        int taken = 1e9;
+        if (coins[idx] <= amount)
+            taken = 1 + fn(coins, amount - coins[idx], dp, idx);
+        return dp[idx][amount] = min(notTaken, taken);
+    }
+
+    int coinChange(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int mini = fn(coins, amount, dp, n - 1);
+        return (mini >= 1e9) ? -1 : mini;
+    }
+};
