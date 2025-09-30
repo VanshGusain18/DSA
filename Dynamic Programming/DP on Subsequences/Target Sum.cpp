21f112fe4a3fb6a8d30038649c9c86cd4578d7a3
[vanshgusain] <vanshgusain18@gmail.com>
2025-09-30 21:55:55 +0530

    Target Sum

--- Dynamic Programming/DP on Subsequences/Target Sum.cpp
+++ Dynamic Programming/DP on Subsequences/Target Sum.cpp
@@ -77,3 +77,37 @@ public:
         return countSubsets(nums, n - 1, subsetSum, dp);
     }
 };
+
+// Tabulation Approch
+
+class Solution
+{
+public:
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if ((totalSum + target) % 2 != 0 || abs(target) > totalSum)
+            return 0;
+        int subsetSum = (totalSum + target) / 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(subsetSum + 1, 0));
+        if (nums[0] == 0)
+            dp[0][0] = 2;
+        else
+            dp[0][0] = 1;
+        if (nums[0] != 0 && nums[0] <= subsetSum)
+            dp[0][nums[0]] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            for (int s = 0; s <= subsetSum; s++)
+            {
+                int notTake = dp[i - 1][s];
+                int take = 0;
+                if (nums[i] <= s)
+                    take = dp[i - 1][s - nums[i]];
+                dp[i][s] = take + notTake;
+            }
+        }
+        return dp[n - 1][subsetSum];
+    }
+};
