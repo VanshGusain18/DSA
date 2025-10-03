8529bdac1453ff57bff88302d651e9584192a315
[vanshgusain] <vanshgusain18@gmail.com>
2025-10-03 16:18:45 +0530

    Edit Distance

--- Dynamic Programming/DP on Strings/Edit Distance.cpp
+++ Dynamic Programming/DP on Strings/Edit Distance.cpp
@@ -52,3 +52,39 @@ public:
         return fn(word1, word2, dp, n - 1, m - 1);
     }
 };
+
+// Tabulation Approch
+
+class Solution
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++)
+        {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    int del = dp[i][j - 1] + 1, ins = dp[i - 1][j] + 1, rep = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = min({del, ins, rep});
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
