31b12064a4e29f0ab6dc7c2df718f85f7ba28760
[vanshgusain] <vanshgusain18@gmail.com>
2025-10-04 11:12:56 +0530

    Best Time to Buy and Sell Stock II

--- Dynamic Programming/DP on Stocks/Best Time to Buy and Sell Stock II.cpp
+++ Dynamic Programming/DP on Stocks/Best Time to Buy and Sell Stock II.cpp
@@ -27,3 +27,36 @@ public:
         return fn(prices, 0, 1);
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(vector<int> &prices, vector<vector<int>> dp, int i, int buy)
+    {
+        if (i == prices.size())
+            return 0;
+        if (dp[i][buy] != -1)
+            return dp[i][buy];
+        if (buy)
+        {
+            int take = -prices[i] + fn(prices, dp, i + 1, 0);
+            int notTake = fn(prices, dp, i + 1, 1);
+            return dp[i][buy] = max(take, notTake);
+        }
+        else
+        {
+            int take = prices[i] + fn(prices, dp, i + 1, 1);
+            int notTake = fn(prices, dp, i + 1, 0);
+            return dp[i][buy] = max(take, notTake);
+        }
+    }
+
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return fn(prices, dp, 0, 1);
+    }
+};
