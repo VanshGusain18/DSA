552e70c6a90c0c3795be2442f0e0d94153a2711c
[vanshgusain] <vanshgusain18@gmail.com>
2025-10-07 18:42:22 +0530

    Best Time to Buy and Sell Stock with Cooldown

--- Dynamic Programming/DP on Stocks/abc.cpp
+++ Dynamic Programming/DP on Stocks/abc.cpp
@@ -31,3 +31,40 @@ public:
         return fn(prices, 0, 1, 0);
     }
 };
+
+// Memoization Approch
+
+class Solution
+{
+public:
+    int fn(vector<int> &prices, vector<vector<vector<int>>> &dp, int i, int buy, int lock)
+    {
+        if (i == prices.size())
+            return 0;
+        if (dp[i][buy][lock] != -1)
+            return dp[i][buy][lock];
+        if (lock)
+        {
+            return dp[i][buy][lock] = fn(prices, dp, i + 1, 1, 0);
+        }
+        if (buy)
+        {
+            int take = -prices[i] + fn(prices, dp, i + 1, 0, 0);
+            int notTake = fn(prices, dp, i + 1, 1, 0);
+            return dp[i][buy][lock] = max(take, notTake);
+        }
+        else
+        {
+            int take = prices[i] + fn(prices, dp, i + 1, 1, 1);
+            int notTake = fn(prices, dp, i + 1, 0, 0);
+            return dp[i][buy][lock] = max(take, notTake);
+        }
+    }
+
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
+        return fn(prices, dp, 0, 1, 0);
+    }
+};
