260d63a04ccb6b127312ab8a8db86af9a93bba83
[vanshgusain] <vanshgusain18@gmail.com>
2025-10-15 19:29:58 +0530

    Longest Increasing Subsequence

--- Dynamic Programming/DP on LIS/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/DP on LIS/Longest Increasing Subsequence.cpp
@@ -75,3 +75,31 @@ public:
         return dp[0][0];
     }
 };
+
+// Binary Search Approch
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int l = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] > temp.back())
+            {
+                temp.push_back(nums[i]);
+                l++;
+            }
+            else
+            {
+                auto idx = lower_bound(temp.begin(), temp.end(), nums[i]);
+                *idx = nums[i];
+            }
+        }
+        return l;
+    }
+};
